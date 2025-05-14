bc90d2bf0b8b26bbdd19e809c54d7953c4023091
avoorhis <avoorhis@mbl.edu>
2025-05-14 14:54:37 -0400

    rm tree from sidebar

--- links-refseq.rb
+++ links-refseq.rb
@@ -18,7 +18,7 @@ require 'json'
 
 $HOMD_URL = "refseq_blast"
 $DB_TYPE = "refseq"
-$homd_url_base = "https://devel.homd.org/"
+$homd_url_base = "https://v4.homd.org/"
 $ANNO = ""
 
 module SequenceServer
