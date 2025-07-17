887b77ce06402a28d07988f4dcf3695416148baf
avoorhis <avoorhis@mbl.edu>
2025-07-17 07:57:49 -0400

    alter for forsyth

---
+++ homd/db_type-genome.rb
@@ -0,0 +1,8 @@
+# Global
+module SequenceServer
+  module DB_Type
+   # $DB_TYPE = "refseq"
+   $HOMD_URL = "genome_blast"
+   $DB_TYPE = "genome"
+  end
+end
---
+++ homd/db_type-refseq.rb
@@ -0,0 +1,7 @@
+# Global
+module SequenceServer
+  module DB_Type
+   $HOMD_URL = "refseq_blast"
+   $DB_TYPE = "refseq"
+  end
+end
