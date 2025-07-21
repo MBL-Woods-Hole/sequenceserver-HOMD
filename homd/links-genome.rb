0eab84baa4f0eb611e26400afa3c3ed5c3268ea5
avoorhis <avoorhis@mbl.edu>
2025-07-21 07:38:14 -0400

    testing logger

--- homd/links-genome.rb
+++ homd/links-genome.rb
@@ -25,6 +25,7 @@ module SequenceServer
 	module Links
 	
         def hmt
+            puts 'ID: '+id
             hmtMatchData = title.match /(HMT-\d{3})/
             hmt = hmtMatchData[1]
             homdurl = $homd_url_base+"/taxa/tax_description?otid=" +hmt.split('-')[1]
