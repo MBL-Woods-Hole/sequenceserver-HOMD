eca760f655a9d2b66f6f7526e7cafb33446b7bfc
avoorhis <avoorhis@mbl.edu>
2025-07-29 18:48:30 -0400

    send temp file

--- lib/sequenceserver/routes.rb
+++ lib/sequenceserver/routes.rb
@@ -213,9 +213,9 @@ module SequenceServer
       #x = Report.generate(job).to_json
       xhash = Report.generate(job)
       #x['querydb']['name'] = x[0]  # since we always only use one db
-      logger.info "xprogram-blastn?: #{xhash['program']}"
-      logger.info xhash['program']
-      logger.info xhash[program]
+      logger.info "xprogram-start: #{xhash}"
+      logger.info "END"
+      #logger.info xhash[program]
       hits = xhash['queries'][0]['hits']  # length will be > 1 if more than one seq entered
       
       logger.info "x: #{hits}"
