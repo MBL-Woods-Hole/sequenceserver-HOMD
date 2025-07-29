6d0bb5918305d18e9fdc56736e3fddb327d40c52
avoorhis <avoorhis@mbl.edu>
2025-07-29 09:40:30 -0400

    send temp file

--- lib/sequenceserver/routes.rb
+++ lib/sequenceserver/routes.rb
@@ -234,7 +234,7 @@ module SequenceServer
         logger.info "3-path: #{fpath}" 
         results.each do |row|
            #f.write("write your stuff here")
-           f.puts "#{row.genome_id}\t#{row.otid}"
+           f.puts "#{row['genome_id']}\t#{row['otid']}"
         end
       end
       
