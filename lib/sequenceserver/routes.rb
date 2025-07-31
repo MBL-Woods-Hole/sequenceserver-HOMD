ed963335ed101043e90cb2a7f31faa4ad21adfb4
avoorhis <avoorhis@mbl.edu>
2025-07-31 12:14:50 -0400

    add excel to gemfile

--- lib/sequenceserver/routes.rb
+++ lib/sequenceserver/routes.rb
@@ -459,14 +459,43 @@ module SequenceServer
         worksheet.write(2,24,"HOMD-Subspecies")
         worksheet.write(2,25,"HOMD-Strain")
         
-        row = 4
+        row = 3
         big_array.each_with_index do |el,i|
-            col = i
-            #f.puts "#{el[:query_num]}\t#{el[:gid]}\t#{el[:hmt]}\t#{el[:hit_title]}\t#{el[:hit_num]}\t#{el[:hit_length]}\t#{el[:hit_qcov]}\t#{el[:hit_tscore]}\t#{el[:hit_evalue]}\t#{el[:hit_ident]}\t#{el[:hit_ipct]}\t#{el[:hsp_num]}\t#{el[:hsp_score]}\t#{el[:hsp_bitscore]}\t#{el[:hsp_evalue]}\t#{el[:hsp_ident]}\t#{el[:hsp_gaps]}\t#{el[:domain]}\t#{el[:phylum]}\t#{el[:class]}\t#{el[:order]}\t#{el[:family]}\t#{el[:genus]}\t#{el[:species]}\t#{el[:subspecies]}\t#{el[:strain]}"
+            col = 0
+            #f.puts "#{el[:query_num]}\t#{el[:gid]}\t#{el[:hmt]}\t#{el[:hit_title]}\t#{el[:hit_num]}\t#{el[:hit_length]}\t#{el[:hit_qcov]}\t#
+            #{el[:hit_tscore]}\t#{el[:hit_evalue]}\t#{el[:hit_ident]}\t#{el[:hit_ipct]}\t#{el[:hsp_num]}\t#{el[:hsp_score]}\t#{el[:hsp_bitscore]}
+            #\t#{el[:hsp_evalue]}\t#{el[:hsp_ident]}\t#{el[:hsp_gaps]}\t#{el[:domain]}\t#{el[:phylum]}\t#{el[:class]}\t#{el[:order]}\t#{el[:family]}\t#{el[:genus]}\t#{el[:species]}\t#{el[:subspecies]}\t#{el[:strain]}"
             worksheet.write(row,col,el[:query_num])
             worksheet.write(row,col+1,el[:gid])
             worksheet.write(row,col+2,el[:hmt])
             worksheet.write(row,col+3,el[:hit_title])
+            worksheet.write(row,col+4,el[:hit_num])
+            worksheet.write(row,col+5,el[:hit_length])
+            worksheet.write(row,col+6,el[:hit_qcov])
+            worksheet.write(row,col+7,el[:hit_tscore])
+            worksheet.write(row,col+8,el[:hit_evalue])
+            worksheet.write(row,col+9,el[:hit_ident])
+            worksheet.write(row,col+10,el[:hit_ipct])
+            worksheet.write(row,col+11,el[:hsp_num])
+            worksheet.write(row,col+12,el[:hsp_score])
+            worksheet.write(row,col+13,el[:hsp_bitscore])
+            worksheet.write(row,col+14,el[:hsp_evalue])
+            worksheet.write(row,col+15,el[:hsp_ident])
+            worksheet.write(row,col+16,el[:hsp_gaps])
+            worksheet.write(row,col+17,el[:domain])
+            worksheet.write(row,col+18,el[:phylum])
+            worksheet.write(row,col+19,el[:class])
+            worksheet.write(row,col+20,el[:order])
+            worksheet.write(row,col+21,el[:family])
+            worksheet.write(row,col+22,el[:genus])
+            worksheet.write(row,col+23,el[:species])
+            worksheet.write(row,col+24,el[:subspecies])
+            worksheet.write(row,col+25,el[:strain])
+            
+            
+            
+            
+            
             row += 1
         end
         
