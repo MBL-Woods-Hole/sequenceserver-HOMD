3835627213f3350a92ba0f0a62e2aedd34d753c5
avoorhis <avoorhis@mbl.edu>
2025-07-31 11:58:42 -0400

    add excel to gemfile

--- lib/sequenceserver/routes.rb
+++ lib/sequenceserver/routes.rb
@@ -321,8 +321,8 @@ module SequenceServer
         #  Now we have taxonomy hash and BLAST big_array
         #
         newHash = eval(Xhash)
-        header = "### DATABASE: #{newHash[:querydb][0][:title]}\n"
-        header += "### PROGRAM: #{newHash[:program_version]}\n"
+        headerA1 = "### DATABASE: #{newHash[:querydb][0][:title]}"
+        headerA2 = "### PROGRAM: #{newHash[:program_version]}"
         query_ary = newHash[:queries]
         big_array = [] # an array of hashes
         
@@ -430,13 +430,52 @@ module SequenceServer
       
       
       
-        worksheet.write('A1',header)
+        worksheet.write('A1',headerA1)
+        worksheet.write('A2',headerA2)
+        worksheet.write(3,1,"Query_num")
+        worksheet.write(3,2,"Genome-ID")
+        worksheet.write(3,3,"HMT-ID")
+        worksheet.write(3,4,"Hit_title")
+        worksheet.write(3,5,"Hit_num")
+        worksheet.write(3,6,"Hit_length")
+        worksheet.write(3,7,"Hit_qcov")
+        worksheet.write(3,8,"Hit_tscore")
+        worksheet.write(3,9,"Hit_evalue")
+        worksheet.write(3,10,"Hit_ident")
+        worksheet.write(3,11,"Hit_Ident(%)")
+        worksheet.write(3,12,"Hsp_num")
+        worksheet.write(3,13,"Hsp_score")
+        worksheet.write(3,14,"Hsp_bitscore")
+        worksheet.write(3,15,"Hsp_eval")
+        worksheet.write(3,16,"Hsp_ident")
+        worksheet.write(3,17,"Hsp_gaps")
+        worksheet.write(3,18,"HOMD-Domain")
+        worksheet.write(3,19,"HOMD-Phylum")
+        worksheet.write(3,20,"HOMD-Class")
+        worksheet.write(3,21,"HOMD-Order")
+        worksheet.write(3,22,"HOMD-Family")
+        worksheet.write(3,23,"HOMD-Genus")
+        worksheet.write(3,24,"HOMD-Species")
+        worksheet.write(3,25,"HOMD-Subspecies")
+        worksheet.write(3,26,"HOMD-Strain")
+        
+        row = 4
+        big_array.each_with_index do |el,i|
+            col = i + 1
+            #f.puts "#{el[:query_num]}\t#{el[:gid]}\t#{el[:hmt]}\t#{el[:hit_title]}\t#{el[:hit_num]}\t#{el[:hit_length]}\t#{el[:hit_qcov]}\t#{el[:hit_tscore]}\t#{el[:hit_evalue]}\t#{el[:hit_ident]}\t#{el[:hit_ipct]}\t#{el[:hsp_num]}\t#{el[:hsp_score]}\t#{el[:hsp_bitscore]}\t#{el[:hsp_evalue]}\t#{el[:hsp_ident]}\t#{el[:hsp_gaps]}\t#{el[:domain]}\t#{el[:phylum]}\t#{el[:class]}\t#{el[:order]}\t#{el[:family]}\t#{el[:genus]}\t#{el[:species]}\t#{el[:subspecies]}\t#{el[:strain]}"
+            worksheet.write(row,col,el[:query_num])
+            worksheet.write(row,col+1,el[:gid])
+            worksheet.write(row,col+2,el[:hmt])
+            worksheet.write(row,col+3,el[:hit_title])
+            row += 1
+        end
         
         workbook.close()
 #       File.open(fpath_out, 'w') do |f|
 #         #f.puts "Genome-ID\tHMT-ID\tDomain\tPhylum\tClass\tOrder\tFamily\tGenus\tSpecies\tSubspecies\tStrain"
 #         #f.puts "Genome-ID\tQuery_num\tHit_title\tHit_num\tHit_length\tHit_qcov\tHit_tscore\tHit_evalue\tHit_ident\tHit_Ident(%)\tHsp_num\tHsp_score\tHsp_bitscore\tHsp_eval\tHsp_ident\tHsp_gaps\tHMT-ID\tDomain\tPhylum\tClass\tOrder\tFamily\tGenus\tSpecies\tSubspecies\tStrain"
 #         f.write header
+
 #         f.write "Query_num\tGenome-ID\tHMT-ID\tHit_title\tHit_num\tHit_length\tHit_qcov\tHit_tscore\t"
 #         f.write "Hit_evalue\tHit_ident\tHit_Ident(%)\tHsp_num\tHsp_score\tHsp_bitscore\tHsp_eval\tHsp_ident\tHsp_gaps\t"
 #         f.write "HOMD-Domain\tHOMD-Phylum\tHOMD-Class\tHOMD-Order\tHOMD-Family\tHOMD-Genus\tHOMD-Species\tHOMD-Subspecies\tStrain\n"
