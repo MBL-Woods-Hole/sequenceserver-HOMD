92485ce8fddb6bef9a6e8097024fe2b4fd96c3b9
avoorhis <avoorhis@mbl.edu>
2025-07-31 11:17:04 -0400

    send temp file

--- lib/sequenceserver/routes.rb
+++ lib/sequenceserver/routes.rb
@@ -9,7 +9,9 @@ require 'sequenceserver/report'
 require 'sequenceserver/database'
 require 'sequenceserver/sequence'
 require 'sequenceserver/makeblastdb'
+
 require 'time'
+require 'write_xlsx'
 
 
 module SequenceServer
@@ -212,11 +214,16 @@ module SequenceServer
         job_id = params['job_id']
         job = Job.fetch(job_id)
         # file written to this data directory
-        fpath_out = File.join(DOTDIR, job_id, 'custom_homd_taxonomy.csv')
-      
-      #
-      # First get GIDS and Taxonomy from MySQL DB
-      #
+        #fpath_out_csv = File.join(DOTDIR, job_id, 'custom_homd_taxonomy.csv')
+        fpath_out_xlsx = File.join(DOTDIR, job_id, 'custom_homd_taxonomy.xlsx')
+        # Create a new Excel workbook
+        workbook = WriteXLSX.new(fpath_out_xlsx)
+        # Add a worksheet
+        worksheet = workbook.add_worksheet
+        
+        #
+        # First get GIDS and Taxonomy from MySQL DB
+        #
         mysql_ids = Array.new
         gids_list = ''
         logger.info "xSEQ IDS= #{sequence_ids}"
@@ -421,25 +428,30 @@ module SequenceServer
         end
 
       
-      File.open(fpath_out, 'w') do |f|
-        #f.puts "Genome-ID\tHMT-ID\tDomain\tPhylum\tClass\tOrder\tFamily\tGenus\tSpecies\tSubspecies\tStrain"
-        #f.puts "Genome-ID\tQuery_num\tHit_title\tHit_num\tHit_length\tHit_qcov\tHit_tscore\tHit_evalue\tHit_ident\tHit_Ident(%)\tHsp_num\tHsp_score\tHsp_bitscore\tHsp_eval\tHsp_ident\tHsp_gaps\tHMT-ID\tDomain\tPhylum\tClass\tOrder\tFamily\tGenus\tSpecies\tSubspecies\tStrain"
-        f.write header
-        f.write "Query_num\tGenome-ID\tHMT-ID\tHit_title\tHit_num\tHit_length\tHit_qcov\tHit_tscore\t"
-        f.write "Hit_evalue\tHit_ident\tHit_Ident(%)\tHsp_num\tHsp_score\tHsp_bitscore\tHsp_eval\tHsp_ident\tHsp_gaps\t"
-        f.write "HOMD-Domain\tHOMD-Phylum\tHOMD-Class\tHOMD-Order\tHOMD-Family\tHOMD-Genus\tHOMD-Species\tHOMD-Subspecies\tStrain\n"
-        #gid,hit_def,hit#,hit_length,qcov,tscore,evalue,%ident, hsp#,hsp_score,hsp_evalue,hsp_ident,hsp_gaps,hps_strand,HMT,TAXONOMY}
-        big_array.each do |el|
-           f.puts "#{el[:query_num]}\t#{el[:gid]}\t#{el[:hmt]}\t#{el[:hit_title]}\t#{el[:hit_num]}\t#{el[:hit_length]}\t#{el[:hit_qcov]}\t#{el[:hit_tscore]}\t#{el[:hit_evalue]}\t#{el[:hit_ident]}\t#{el[:hit_ipct]}\t#{el[:hsp_num]}\t#{el[:hsp_score]}\t#{el[:hsp_bitscore]}\t#{el[:hsp_evalue]}\t#{el[:hsp_ident]}\t#{el[:hsp_gaps]}\t#{el[:domain]}\t#{el[:phylum]}\t#{el[:class]}\t#{el[:order]}\t#{el[:family]}\t#{el[:genus]}\t#{el[:species]}\t#{el[:subspecies]}\t#{el[:strain]}"
-        end
+      
+      
+        worksheet.write('A1',header)
         
-        
-      end
+        worksheet.close()
+#       File.open(fpath_out, 'w') do |f|
+#         #f.puts "Genome-ID\tHMT-ID\tDomain\tPhylum\tClass\tOrder\tFamily\tGenus\tSpecies\tSubspecies\tStrain"
+#         #f.puts "Genome-ID\tQuery_num\tHit_title\tHit_num\tHit_length\tHit_qcov\tHit_tscore\tHit_evalue\tHit_ident\tHit_Ident(%)\tHsp_num\tHsp_score\tHsp_bitscore\tHsp_eval\tHsp_ident\tHsp_gaps\tHMT-ID\tDomain\tPhylum\tClass\tOrder\tFamily\tGenus\tSpecies\tSubspecies\tStrain"
+#         f.write header
+#         f.write "Query_num\tGenome-ID\tHMT-ID\tHit_title\tHit_num\tHit_length\tHit_qcov\tHit_tscore\t"
+#         f.write "Hit_evalue\tHit_ident\tHit_Ident(%)\tHsp_num\tHsp_score\tHsp_bitscore\tHsp_eval\tHsp_ident\tHsp_gaps\t"
+#         f.write "HOMD-Domain\tHOMD-Phylum\tHOMD-Class\tHOMD-Order\tHOMD-Family\tHOMD-Genus\tHOMD-Species\tHOMD-Subspecies\tStrain\n"
+#         #gid,hit_def,hit#,hit_length,qcov,tscore,evalue,%ident, hsp#,hsp_score,hsp_evalue,hsp_ident,hsp_gaps,hps_strand,HMT,TAXONOMY}
+#         big_array.each do |el|
+#            f.puts "#{el[:query_num]}\t#{el[:gid]}\t#{el[:hmt]}\t#{el[:hit_title]}\t#{el[:hit_num]}\t#{el[:hit_length]}\t#{el[:hit_qcov]}\t#{el[:hit_tscore]}\t#{el[:hit_evalue]}\t#{el[:hit_ident]}\t#{el[:hit_ipct]}\t#{el[:hsp_num]}\t#{el[:hsp_score]}\t#{el[:hsp_bitscore]}\t#{el[:hsp_evalue]}\t#{el[:hsp_ident]}\t#{el[:hsp_gaps]}\t#{el[:domain]}\t#{el[:phylum]}\t#{el[:class]}\t#{el[:order]}\t#{el[:family]}\t#{el[:genus]}\t#{el[:species]}\t#{el[:subspecies]}\t#{el[:strain]}"
+#         end
+#       end
+      
+      
       current_time = Time.now
       filename_datetime = current_time.strftime("%Y%m%d_%H%M%S") 
       send_file fpath_out, 
-              type: 'text/csv', 
-              filename: "custom_homd_taxonomy_#{filename_datetime}.csv", 
+              type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 
+              filename: "custom_homd_taxonomy_#{filename_datetime}.xlsx", 
               disposition: 'attachment' 
      
     end
