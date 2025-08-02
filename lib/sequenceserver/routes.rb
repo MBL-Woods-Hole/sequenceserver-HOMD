f116411dbc372eb67edc73c9e90004f6338c4ec4
avoorhis <avoorhis@mbl.edu>
2025-08-01 20:30:06 -0400

    add ba length

--- lib/sequenceserver/routes.rb
+++ lib/sequenceserver/routes.rb
@@ -599,9 +599,9 @@ module SequenceServer
                 worksheet.write(row,col+23,el[:species])
                 worksheet.write(row,col+24,el[:subspecies])
                 worksheet.write(row,col+25,el[:strain])
-                if big_array.length == 1
-                    workbook.close()
-                end
+                
+                break if big_array.length == 1
+                    
                 row += 1
             end
             
