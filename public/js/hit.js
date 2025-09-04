1b42ecd911e30a0a2c347d4bade952b4236bdf83
avoorhis <avoorhis@mbl.edu>
2025-09-04 17:27:29 -0400

    add sp to hit

--- public/js/hit.js
+++ public/js/hit.js
@@ -114,7 +114,7 @@ export default class extends Component {
         return <div className="section-header">
             <h4>
                 <i className="fa fa-minus-square-o"></i>&nbsp;
-                <strong>{this.props.hit.id} 'XX' {this.props.hit.species}</strong>&nbsp;
+                <strong>{this.props.hit.id}  {this.props.hit.species}</strong>&nbsp;
                 {this.props.hit.title}
             </h4>
             <span className="label label-reset pos-label">{meta}</span>
